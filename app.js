629ecbee462a21b78061d174385cf3875c262d3c
Matthew Carson <matthew.carson@pm.me>
2022-03-23 12:51:03 -0400

    Style the time blocks based on the current time of day.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// Set classes on the time blocks based on the time.
+(() => {
+  const time = new Date
+  time.getHours()
+
+  const timeBlocks = $('.time-block')
+  console.log(timeBlocks.toArray())
+  timeBlocks.toArray().forEach(timeBlock => {
+    if (timeBlock.dataset.hour < time.getHours()) $(timeBlock).children('p').addClass('past')
+    if (timeBlock.dataset.hour == time.getHours()) $(timeBlock).children('p').addClass('present')
+    if (timeBlock.dataset.hour > time.getHours()) $(timeBlock).children('p').addClass('future')
+  })
+  console.log(timeBlocks)
+})()
+
 $('button.save').click(() => console.log('saving...'))
